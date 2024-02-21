f1079ee43d7656141d03b5b22247e548a07dae93
chnwin005 <chnwin005@myuct.ac.za>
2024-02-21 13:15:48 +0200

    Chart Colours Fixed

--- election-app/src/app/page.tsx
+++ election-app/src/app/page.tsx
@@ -146,15 +146,15 @@ const Graphs = () => {
     });
     const uniqueProvinces = Array.from(provinces); // Convert set to array
     const colors = [
-      'rgba(255, 159, 64, 1)',
       'rgba(75, 192, 192, 1)',
-      'rgba(153, 102, 255, 1)',
-      'rgba(0, 255, 0, 1)',
+      'rgba(255, 0, 0, 1)',
       'rgba(255, 99, 132, 1)',
+      'rgba(0, 255, 0, 1)',
+      'rgba(255, 159, 64, 1)',
+      'rgba(255, 205, 86, 1)',
+      'rgba(153, 102, 255, 1)',
       'rgba(128, 0, 128, 1)',
       'rgba(54, 162, 235, 1)',
-      'rgba(255, 0, 0, 1)',
-      'rgba(255, 205, 86, 1)',
     ]; // Define your own color scheme or use Chart.js built-in color schemes
   
     const sharedColorMap = {}; // Shared color map for provinces
